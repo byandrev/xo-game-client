1937cf413a3b8aacbc0c21c38d4bc33d46cb61ff
byandrev <byandrev@gmail.com>
2023-05-20 17:09:34 -0500

    set light theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,14 @@
 const light = {
-  primary: "red"
+  primary: "#3070F7",
+  primaryLight: "#4081F6",
+  secondary: "#f67732",
+  secondaryLight: "#f3b457",
+  text: "#777777",
+  shadow: "#DEDDDD"
 };
 
 const dark = {
-  primary: "green"
+  primary: "#3070F7"
 };
 
 export { light, dark };
